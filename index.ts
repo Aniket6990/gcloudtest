379e2372d171236ad7e77bd9c91acbddbd4ae8b1
Aniket Singh <aniketsingh6990@gmail.com>
2024-03-02 15:35:33 +0530

    add health endpoint 🐯

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("<h1>hello world<h1/>");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Tiger zinda hai..." });
+});
+
 app.listen(4000, () => {
   console.log("server is listening of port 4000");
 });
